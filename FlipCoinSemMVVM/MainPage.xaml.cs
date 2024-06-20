eed4668fc51347759b7af6dc78bac819f37e153e
PedroLudovic144 <pedroludovic5@gmail.com>
2024-06-20 20:07:22 -0300

    New commit

---
+++ FlipCoinSemMVVM/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace FlipCoinSemMVVM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnFlipCoinClicked(object sender, EventArgs e)
+        {
+            if (PickerChoice.SelectedIndex == -1)
+            {
+                DisplayAlert("Erro", "Por favor, selecione Cara ou Coroa", "OK");
+                return;
+            }
+
+            string userChoice = PickerChoice.SelectedItem.ToString();
+            string[] choices = { "Cara", "Coroa" };
+            Random random = new Random();
+            string coinFlipResult = choices[random.Next(choices.Length)];
+
+            if (userChoice == coinFlipResult)
+            {
+                ResultLabel.Text = "Você acertou! Deu " + coinFlipResult;
+            }
+            else
+            {
+                ResultLabel.Text = "Você errou! Deu " + coinFlipResult;
+            }
+
+            // Definir a imagem correspondente
+            if (coinFlipResult == "Cara")
+            {
+                ResultImage.Source = "cara.png";
+            }
+            else
+            {
+                ResultImage.Source = "coroa.png";
+            }
+            ResultImage.IsVisible = true;
+        }
+    }
+}
---
+++ FlipCoinSemMVVM/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FlipCoinSemMVVM
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
